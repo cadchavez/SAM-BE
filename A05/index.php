fb759c0fa9bbc2d018a08651e39a545a53f1e057
Christell Chavez <ca.chavez0516@gmail.com>
2024-12-20 23:27:31 +0800

    Faith Haven card done

--- A05/index.php
+++ A05/index.php
@@ -1,4 +1,5 @@
 <?php include 'connect.php'; ?>
+
 <!DOCTYPE html>
 <html>
 
@@ -36,6 +37,53 @@ and is wrapped around the whole page content, except for the footer in this exam
             <p>Welcome to my <span class="w3-tag">sea of memories</span></p>
         </header>
 
+        <!-- Faith Haven Card -->
+        <div class="w3-row">
+            <div class="w3-col l8 s12">
+                <!-- Faith Haven Card -->
+                <div class="w3-card-4 w3-margin w3-white">
+                    <?php
+                    // Query to get the details of "Faith Haven"
+                    $query = "SELECT * FROM islandsOfPersonality WHERE name = 'Faith Haven'";
+                    $result = executeQuery($query);
+
+                    // Check if any data is returned
+                    if ($row = mysqli_fetch_array($result)) {
+                        ?>
+                        <div class="card">
+                            <!-- Title -->
+                            <div class="w3-container">
+                                <h3><b><?php echo $row['name']; ?></b></h3>
+                                <!-- Long Description -->
+                                <h5><?php echo $row['longDescription']; ?></h5>
+
+                                <div class="w3-row">
+                                    <?php
+                                    // Query to get the images associated with Faith Haven
+                                    $imageQuery = "SELECT * FROM islandcontents WHERE islandOfPersonalityID = " . $row['islandOfPersonalityID'];
+                                    $imageResult = executeQuery($imageQuery);
+                                    while ($imageRow = mysqli_fetch_array($imageResult)) {
+                            
+                                    $imageFile = $imageRow['image'];
+                                    $content = $imageRow['content']; 
+                                        ?> 
+                                        <div class="w3-col l12 m6 s12" style="display: flex; flex-direction: column; align-items: center; justify-content: center; margin-bottom: 30px;">
+                                            <img src="images/<?php echo $imageFile; ?>" alt="Faith Haven Image" style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                                            <p style="text-align: center; margin-top: 10px;"><?php echo $content; ?></p> <!-- Display the content from islandcontents -->
+                                        </div>
+                                        <?php
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                </div>
+                    <?php
+                    }
+                    ?>
+            </div>
+        </div>
+    </div>
+
         <!-- Grid -->
         <div class="w3-row">
 
@@ -55,16 +103,6 @@ and is wrapped around the whole page content, except for the footer in this exam
                             ultricies mi non congue ullam corper. Praesent tincidunt sed
                             tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida
                             diam non fringilla.</p>
-                        <div class="w3-row">
-                            <div class="w3-col m8 s12">
-                                <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE
-                                            »</b></button></p>
-                            </div>
-                            <div class="w3-col m4 w3-hide-small">
-                                <p><span class="w3-padding-large w3-right"><b>Comments  </b> <span
-                                            class="w3-tag">0</span></span></p>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <hr>
@@ -102,11 +140,10 @@ and is wrapped around the whole page content, except for the footer in this exam
             <div class="w3-col l4">
                 <!-- About Card -->
                 <div class="w3-card w3-margin w3-margin-top">
-                    <img src="/w3images/avatar_g.jpg" style="width:100%">
+                    <img src="../A05/images/self.png" style="width:100%">
                     <div class="w3-container w3-white">
-                        <h4><b>My Name</b></h4>
-                        <p>Just me, myself and I, exploring the universe of uknownment. I have a heart of love and a
-                            interest of lorem ipsum and mauris neque quam blog. I want to share my world with you.</p>
+                        <h4><b>Christell Angelica</b></h4>
+                        <p>Hello! I’m Christell Angelica Chavez, a 22-year-old student and babysitter from San Pablo, Sto. Tomas. I’m someone who values faith, family, hobbies, and work, as these define who I am. Whether I’m nurturing young minds or pursuing my passions, I approach life with dedication and care.<br><br>This website is a window into the moments and memories that shape me. Each section reflects an island of my personality, offering a glimpse into what inspires me, keeps me grounded, and drives me forward. <br><br>Welcome to a collection of experiences that make me, me.</p>
                     </div>
                 </div>
                 <hr>
